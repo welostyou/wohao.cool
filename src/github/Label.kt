9089bee66d766853ee2d4a6106dc486f80e8932a
13567586753@163.com <hello.95>
2019-05-23 00:00:10 +0800

    QAQ

--- src/github/Label.kt
+++ src/github/Label.kt
@@ -1,3 +1,7 @@
 package github
 
 data class Label(val id: Long, val name: String, val color: String)
+
+val List<Label>.database get() = joinToString(",") { it.id.toString() }
+
+val String.labels get() = split(",").map(String::toLong)
